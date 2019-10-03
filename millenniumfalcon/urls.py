f2ca4db268c7280911c4db379d19e8e860cbaa66
Florian Ortbauer <florian.ortbauer@edu.campus02.at>
2019-10-03 20:26:06 +0200

    Finally Fixed REST

--- millenniumfalcon/urls.py
+++ millenniumfalcon/urls.py
@@ -1,6 +1,16 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+]
+
+urlpatterns +=[
+    path(r'clients', views.ClientAPIView.as_view(), name='client-list'),
+    path(r'sites', views.SiteAPIView.as_view(), name='site-list'),
+    path(r'areas', views.AreaAPIView.as_view(), name='area-list'),
+    path(r'systems', views.SystemAPIView.as_view(), name='system-list'),
+    path(r'components', views.ComponentAPIView.as_view(), name='component-list'),
+    path(r'missions', views.MissionAPIView.as_view(), name='mission-list'),
 ]
