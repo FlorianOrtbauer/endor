0340fda5520deeb79dd1d842647355454cd8e9d2
Florian ORTBAUER <Florian.Ortbauer@knapp.com>
2019-10-03 15:30:26 +0200

    Added cron process sceleton

---
+++ millenniumfalcon/management/commands/checktasks.py
@@ -0,0 +1,3 @@
+# ToDo: Define and implement tidy up logic of tasks
+#       -> check if Active tasks still to be active?
+#       -> check task priorities and potentially auto-increase priority of 'forgotten' tasks
